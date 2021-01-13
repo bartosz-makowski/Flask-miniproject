d0e3ab02faacebe08113c6ea6ab7a54f3d4a31a8
Bartek_makowski <mako19895@gmail.com>
2021-01-13 01:08:44 +0000

    added working login check

--- app.py
+++ app.py
@@ -46,6 +46,32 @@ def register():
     return render_template("register.html")
 
 
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #check if user is in DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect username")
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
 
     app.run(host=os.environ.get('IP'),
