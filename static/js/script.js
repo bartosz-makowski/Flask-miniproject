418cc09e051cc88090c88b46cc15d890992db198
Bartek_makowski <mako19895@gmail.com>
2021-01-14 02:01:53 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "select"
+        }
+    });
   });
 
   
