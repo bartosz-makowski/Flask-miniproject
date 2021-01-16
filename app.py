542308ae6ef55b49122fc2041fe7edf26799da8e
Bartek_makowski <mako19895@gmail.com>
2021-01-16 16:22:47 +0000

    added option to add new category to db

--- app.py
+++ app.py
@@ -141,6 +141,25 @@ def delete_task(task_id):
     flash(("Task deleted"))
     return redirect(url_for('get_tasks'))
 
+
+@app.route('/get_categories')
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name",1))
+    return render_template('categories.html', categories=categories)
+
+
+@app.route('/add_category', methods=["GET", "POST"])
+def add_category():
+    if request.method == 'POST':
+        category = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.insert_one(category)
+        flash("New category added")
+        return redirect(url_for('get_categories'))
+
+    return render_template('add_category.html')
+
 if __name__ == '__main__':
 
     app.run(host=os.environ.get('IP'),
